d9b4853ba95680a40d50e22b3f5e919147138bd1
Angelo Maramara <angelo.maramara@gmail.com>
2016-07-28 00:11:14 -0400

    Add custom jQuery and explanations

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+
+    // This is the Fullpage jQuery plugin that provides an smooth transtion between sections on your page. It also helps to keep the page at full height and width of the browser so there is no unncessary scrolling.
     $('#fullpage').fullpage();
+
+    // This is the Splitchar jQuery Plugin that splits the characters of an element and allows you to style them individually. I applied this to all of the section headings of my Resume.
     $('.splitchar').splitchar();
+
+    // This is my custom jQuery code that doubles the social media icons when hovered upon and returns it back to it's initial size when no longer hovered upon.
+    $('.social').hover(function () {
+        $(this).css('transform', 'scale(1.5,1.5)');
+    }, function () {
+        $(this).css('transform', 'scale(1,1)');
+    });
 });
